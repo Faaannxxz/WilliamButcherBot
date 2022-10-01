a8a67ab8fb7784f537ff98da27c8c8d3a1f5989a
Pendragon <stayinherewthme@gmail.com>
2022-10-02 03:26:35 +0700

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+BOT_TOKEN = "5701341223:AAHQL19XLCpyNXHtHmoLDMAvirFYEKZbahc"
+API_ID = 14314614
+API_HASH = "7f030d708946ee907bf0944c05c8cc09"
+USERBOT_PREFIX = "."
+PHONE_NUMBER = "+6285894085634"  # Need for Userbot
+SUDO_USERS_ID = [
+    5035563632,
+    5451027258,
+]  # Sudo users have full access to everything, don't trust anyone
+LOG_GROUP_ID = -1001729421519
+GBAN_LOG_GROUP_ID = -1001729421519
+MESSAGE_DUMP_CHAT = -1001729421519
+WELCOME_DELAY_KICK_SEC = 300
+MONGO_URL = "mongodb+srv://maticnemanja:kucingkawin@cluster0.qhwwh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+ARQ_API_KEY = "KHJZGM-OXGZRB-WLJFAW-VKMQOV-ARQ"
+ARQ_API_URL = "https://arq.hamker.in"
+LOG_MENTIONS = True
+RSS_DELAY = 300  # In seconds
+PM_PERMIT = True
